000f6ec758986acb0602e3dbfcc08150c5fac94b
yhao <yhao@ideabinder.com>
2016-10-14 18:34:03 +0800

    增message插件

--- www/components/message/message.js
+++ www/components/message/message.js
@@ -3,20 +3,18 @@
         console.error("require zepto");
         return;
     }
-    if (!window.hy
-        || !window.hy.core
-        || Object.getOwnPropertyNames(window.hy.core).length === 0) {
-        console.error("require window.hy.core");
-        return;
-    }
+    // if (!window.hy
+    //     || !window.hy.core
+    //     || Object.getOwnPropertyNames(window.hy.core).length === 0) {
+    //     console.error("require window.hy.core");
+    //     return;
+    // }
 
 
     window.hy = window.hy || {};
     window.hy.message = function (cfg) {
         var core = window.hy.core;
-        var _cfg = $.extend(true, {
-            // message id
-            id: core.getRandom(),
+        var _cfg = $.extend({
             //外层box
             element: $("<div>"),
             //显示内容
@@ -25,11 +23,41 @@
             duration: 1.5
         }, cfg);
 
+        var _this = null;
+        var _element = _cfg.element;
+
+        init();
+
+        function init() {
+            createDom();
+            _this = new message();
+        }
+
+        function createDom() {
+            var $box = $("<div>")
+                .append(
+                    $("<span>").text(_cfg.content)
+                ).addClass("hy_message");
+
+            _element.append($box);
+        }
+
+        function show(content, type) { }
+
+        function hide() { }
+
+        function destory() { }
 
 
+        //实例对象
+        function message() {
+            if (this instanceof message === false) return;
+            this.show = show;
+            this.hide = hide;
+            this.destory = destory;
+            return this;
+        }
+        return _this;
     }
-
-
-
 
 } (window, Zepto))
